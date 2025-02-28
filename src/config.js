0a6855ed4c3a52c561f4ce29d21a056b662151da
Xy <1162015594@qq.com>
2025-02-28 11:19:15 +0800

    AICHAT

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    //apiBaseUrl : 'http://localhost:8088/Aurora'
-    apiBaseUrl : 'http://47.103.60.221:8088/Aurora'
+    apiBaseUrl : 'http://localhost:8088/Aurora'
+    //apiBaseUrl : 'http://47.103.60.221:8088/Aurora'
 }
 
 export default  config
